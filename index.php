3f679f41aaa5931261b02d57ada2628f76176f25
Riverside Rocks <trent@riverside.rocks>
2020-10-21 15:14:42 -0400

    Add basic pug

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require __DIR__ . '/vendor/autoload.php';
 $router = new \Bramus\Router\Router();
 
 $router->get('/', function() {
-    echo "It works!";
+    Phug::displayFile('views/index.pug');
 });
 
 $router->run();
