38022d5b8a22cf2370ac99d23bd1b3ccf054ec70
Riverside Rocks <trent@riverside.rocks>
2020-10-22 15:32:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $pug = new Pug();
 
 
 $router->get('/', function() {
-    Pug::render('views/index.pug', array(
+    Phug::render('views/index.pug', array(
         'site_name' => 'Riverside Rocks'
     ));
 });
