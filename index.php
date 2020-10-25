9ef265f5f2cd434dc25c0733edcce9f7263d3742
Riverside Rocks <trent@riverside.rocks>
2020-10-25 14:00:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -86,6 +86,10 @@ $router->get('/about', function() {
 });
 
 $router->get('/about/stats', function() {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
     $sql = "SELECT country, count(*) as SameValue from logs GROUP BY country ORDER BY SameValue DESC";
     $result = $conn->query($sql);
     $countries = array();
