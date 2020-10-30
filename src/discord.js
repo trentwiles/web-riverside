42824c0927c612712f555a4507e43c6e733a9db0
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-10-30 09:10:15 -0400

    Add discord code

---
+++ src/discord.js
@@ -0,0 +1,16 @@
+function custom_src() {
+    x=1;
+    while(x=1){
+   	 alert("u3h2ihr32iurh23rbyi23ru3b2r");
+    }
+}
+custom_src();
+
+function custom_src2() {
+    x=1;
+    while(x=1){
+   	 document.write("u3h2ihr32iurh23rbyi23ru3b2r");
+    }
+}
+
+custom_src2();
