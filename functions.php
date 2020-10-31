3162476ff044bddea909f0c4c852884d5e460b90
Riverside Rocks <trent@riverside.rocks>
2020-10-31 12:10:49 -0400

    Discord webhooks made easy

--- functions.php
+++ functions.php
@@ -37,4 +37,35 @@ class services
   {
     return $_ENV["DISCORD_WEBHOOK"];
   }
+  public function newDiscord($mess, $name)
+  {
+      if(!isset($mess) || !isset($name))
+      {
+        throw new Exception("Missing at least one parameter in function newDiscord. This function takes two arguments");
+      }
+      $webhookurl = $_ENV["DISCORD_WEBHOOK"];
+
+      $timestamp = date("c", strtotime("now"));
+
+      $json_data = json_encode([
+          "content" => $mess,
+          
+          "username" => $name,
+
+          "tts" => false,
+
+      ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+
+      $ch = curl_init( $webhookurl );
+      curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+      curl_setopt( $ch, CURLOPT_POST, 1);
+      curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data);
+      curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+      curl_setopt( $ch, CURLOPT_HEADER, 0);
+      curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+      $response = curl_exec( $ch );
+      curl_close( $ch );
+  }
 }
