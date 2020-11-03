47d1b09dc1dbf567dcd90609d43842299e778112
Riverside Rocks <trent@riverside.rocks>
2020-11-03 13:17:38 -0500

    set key

--- src/v1/username.php
+++ src/v1/username.php
@@ -1,5 +1,17 @@
 <?php
 session_start();
 header("Content-type: text/javascript");
+
+$username = $_SESSION["username"];
+
+$sql = "SELECT * FROM logins WHERE username='$username'";
+$result = $conn->query($sql);
+if (!empty($result) && $result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $key = $row["temp_auto_api_key"];
+        break;
+    }
+}
+
 echo "const user = '" . $_SESSION["username"] . "';\n";
-echo "const user_id = " . $_SESSION["id"] . ";\n";
+echo "const key = " . $key . ";\n";
