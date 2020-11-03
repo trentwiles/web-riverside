0d54de2a885bf5231201f03462e59b9abce6bb79
Riverside Rocks <trent@riverside.rocks>
2020-11-03 11:47:50 -0500

    Update username.php

--- src/v1/username.php
+++ src/v1/username.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 header("Content-type: text/javascript");
-echo "const user = " . $_SESSION["username"];
-echo "const user_id = " . $_SESSION["id"];
+echo "const user = " . $_SESSION["username"] . ";\n";
+echo "const user_id = " . $_SESSION["id"] . ";\n";
