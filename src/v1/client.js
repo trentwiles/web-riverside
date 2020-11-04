457cfcd1fd998ade876a75132924c9caf390453e
Riverside Rocks <trent@riverside.rocks>
2020-11-04 15:49:23 -0500

    Update client.js

--- src/v1/client.js
+++ src/v1/client.js
@@ -26,9 +26,9 @@ function sendMessage(message, key){
 }
 // Example request: https://riverside.rocks/v1/new?m=hello!&key=abcdefg
 // If the key is valid, OK should be returned
+var content = document.getElementById("mess").value
 input.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
-        var content = document.getElementById("mess").value
         sendMessage(content, key)
     }
 })
