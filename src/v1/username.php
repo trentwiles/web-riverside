35b18266dcf9c63574c990fadcf3fef86bfb7a4a
Riverside Rocks <trent@riverside.rocks>
2020-11-04 11:09:07 -0500

    use mysql

--- src/v1/username.php
+++ src/v1/username.php
@@ -4,6 +4,16 @@ header("Content-type: text/javascript");
 
 $username = $_SESSION["username"];
 
+$servername = $_ENV['MYSQL_SERVER'];
+$username = $_ENV["MYSQL_USERNAME"];
+$password = $_ENV["MYSQL_PASSWORD"];
+$dbname = $_ENV["MYSQL_DATABASE"];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $sql = "SELECT * FROM logins WHERE username='$username'";
 $result = $conn->query($sql);
 if (!empty($result) && $result->num_rows > 0) {
