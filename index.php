f9b4e02dbc5940331480c2cacc53559f13563479
Riverside Rocks <trent@riverside.rocks>
2020-11-04 11:18:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -131,6 +131,10 @@ $router->get('/about/legal', function() {
 });
 
 $router->get('/code/production/cred.js', function() {
+    $servername = $_ENV['MYSQL_SERVER'];
+    $username = $_ENV["MYSQL_USERNAME"];
+    $password = $_ENV["MYSQL_PASSWORD"];
+    $dbname = $_ENV["MYSQL_DATABASE"];
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
