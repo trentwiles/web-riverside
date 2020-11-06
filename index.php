f6b70efa10577d7ffdca941e180449370c7ff04e
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-11-06 11:19:24 -0500

    Legal stuff woo hoo

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+/*
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2020 Trent Wiles and the Riverside Rocks authors       |
+   +----------------------------------------------------------------------+
+   | This source file is subject to the Apache 2.0 Lisence.               |
+   |                                                                      |
+   | If you did not receive a copy of the license and are unable to       |
+   | obtain it through the world-wide-web, please send a email to         |
+   | support@riverside.rocks so we can mail you a copy immediately.       |
+   +----------------------------------------------------------------------+
+   | Authors: Trent "Riverside Rocks" Wiles <trent@riverside.rocks>       |
+   +----------------------------------------------------------------------+
+*/
+
 session_start();
 header("X-Powered-By: Riverside Rocks");
 
