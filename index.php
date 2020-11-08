4c1f065fc8f5c2620fd603868d9bfe5374a82220
Riverside Rocks <trent@riverside.rocks>
2020-11-08 15:39:17 -0500

    Done

--- index.php
+++ index.php
@@ -673,7 +673,24 @@ $router->get('/account/welcome', function() {
 });
 
 $router->post('/account/welcome', function() {
-    
+    if(!isset($_POST["bio"]))
+    {
+        header("Location: /account/dashboard");
+        die();
+    }
+    $servername = $_ENV['MYSQL_SERVER'];
+    $username = $_ENV["MYSQL_USERNAME"];
+    $password = $_ENV["MYSQL_PASSWORD"];
+    $dbname = $_ENV["MYSQL_DATABASE"];
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $updated_bio = $conn -> real_escape_string(htmlspecialchars($_POST["bio"]));
+    $auth_u = $_SESSION["username"];
+    $sql = "UPDATE `logins` SET `bio`='${updated_bio}' WHERE username='$auth_u'";
+    $result = $conn->query($sql);
 });
 
 
