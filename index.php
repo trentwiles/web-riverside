ed1f01bb9a9f39daf44d7d8db3df7ff29418fc68
Riverside Rocks <trent@riverside.rocks>
2020-11-10 09:39:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -180,6 +180,7 @@ $router->get('/code/production/cred.js', function() {
         }
     }
     echo "const key = Cookies.get('key');\n";
+    echo "const channel = \"" . $_SESSION["channel"] . "\";\n";
     echo "const username = \"" . $user . "\";\n";
 });
 
