1fc6f7f6ae74dead11b9bef83ae49202e00fca0f
Riverside Rocks <trent@riverside.rocks>
2020-11-14 10:48:19 -0500

    Bad request

--- index.php
+++ index.php
@@ -307,6 +307,48 @@ $router->get('/admin/upload', function() {
     Phug::displayFile('views/upload.pug');
 });
 
+$router->post('/v1/ugc-handler', function() {
+    $storage = new \Upload\Storage\FileSystem('assets/serve/production/app');
+    $file = new \Upload\File('file-0', $storage);
+
+    // Optionally you can rename the file on upload
+    $new_filename = uniqid();
+    $file->setName($new_filename);
+
+    // Validate file upload
+    // MimeType List => http://www.iana.org/assignments/media-types/media-types.xhtml
+    $file->addValidations(array(
+        // Ensure file is of type "image/png"
+        new \Upload\Validation\Mimetype('image/png'),
+
+        //You can also add multi mimetype validation
+        //new \Upload\Validation\Mimetype(array('image/png', 'image/gif'))
+
+        // Ensure file is no larger than 5M (use "B", "K", M", or "G")
+        new \Upload\Validation\Size('5M')
+    ));
+
+    // Access data about the file that has been uploaded
+    $data = array(
+        'name'       => $file->getNameWithExtension(),
+        'extension'  => $file->getExtension(),
+        'mime'       => $file->getMimetype(),
+        'size'       => $file->getSize(),
+        'md5'        => $file->getMd5(),
+        'dimensions' => $file->getDimensions()
+    );
+
+    // Try to upload file
+    try {
+        // Success!
+        $file->upload();
+    } catch (\Exception $e) {
+        // Fail!
+        $errors = $file->getErrors();
+        header("HTTP/1.1 400 Bad Request");
+    }
+});
+
 $router->post('/admin/upload', function() {
     $pug = new Pug();
     if($_POST["key"] !== $_ENV["UPLOAD"]){
