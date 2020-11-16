4e3672aacf83d0d45686814b1ae12671ede56032
Riverside Rocks <trent@riverside.rocks>
2020-11-16 10:53:21 -0500

    One last test

--- w.php
+++ w.php
@@ -1,14 +1,16 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+use Mike42\Wikitext\WikitextParser;
 
-use Mike42\Wikitext\WikitextParser as wiki;
 $wiki = $_GET["w"];
 
 $wiki_apis = json_decode(file_get_contents("https://wiki.riverside.rocks/api.php?action=query&prop=revisions&titles=${wiki}&rvslots=*&rvprop=content&format=json"), true);
 
 $wiki_content = $wiki_apis["query"]["pages"]["0"]["slots"]["main"]["*"];
 
-$parser = new wiki($wiki_content);
+
+/* The most rudimentary way to invoke the parser */
+$parser = new WikitextParser($wiki_content);
 $output = $parser -> result;
 
-echo $wiki_content;
+echo $output;
