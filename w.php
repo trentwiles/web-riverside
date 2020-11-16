a5b3a8ccf48c1bd9a56d9695aa16627fcdc10b9f
Riverside Rocks <trent@riverside.rocks>
2020-11-16 11:04:49 -0500

    Update wiki parser

--- w.php
+++ w.php
@@ -6,8 +6,19 @@ $wiki = $_GET["w"];
 
 $wiki_apis = json_decode(file_get_contents("https://wiki.riverside.rocks/api.php?action=query&prop=revisions&titles=${wiki}&rvslots=*&rvprop=content&format=json"), true);
 
-$wiki_content = $wiki_apis["query"]["pages"]["3"]["revisions"]["0"]["slots"]["main"]["*"];
-
+$r = 0;
+while(true)
+{
+    if($wiki_apis["query"]["pages"][$r]["revisions"]["0"]["slots"]["main"]["*"] == "")
+    {
+        $r = $r +1;
+    }
+    else
+    {
+        break;
+    }
+}
+$wiki_content = $wiki_apis["query"]["pages"][$r]["revisions"]["0"]["slots"]["main"]["*"];
 
 /* The most rudimentary way to invoke the parser */
 $parser = new WikitextParser($wiki_content);
