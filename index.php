24e10cff7001cff17be2cd7cb29d686175cbd5e7
Riverside Rocks <trent@riverside.rocks>
2020-11-17 08:10:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -71,6 +71,56 @@ END EMERGENCY SHUTOFF SWITCH
 
 $ip = $_SERVER['REMOTE_ADDR'];
 
+$url = $_SERVER["REQUEST_URI"];
+
+    
+    $ua = $_SERVER['HTTP_USER_AGENT'];
+    echo "Before issets <br>";
+    $hacks = Secure::returnExploits();
+    if(isset($hacks[$url])){
+        $mes = "AUTOMATED REPORT: " . $hacks[$url];
+    }
+        echo "Before report <br>";
+    if(isset($mes)){
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $to_discord = "${ip} - ${mes}";
+        Rocks::newDiscord($to_discord, "Hacker Feed");
+        $client = new GuzzleHttp\Client([
+            'base_uri' => 'https://api.abuseipdb.com/api/v2/'
+          ]);
+          echo "Before POST <br>";
+          $response = $client->request('POST', 'report', [
+              'query' => [
+                  'ip' => "${ip}",
+                  'categories' => '15',
+                  'comment' => "${mes}"
+              ],
+              'headers' => [
+                  'Accept' => 'application/json',
+                  'Key' => $_ENV["ABUSE_IP_DB"]
+            ],
+          ]);
+          echo "After send <br>";
+          $output = $response->getBody();
+          // Store response as a PHP object.
+          $ipDetails = json_decode($output, true);
+          echo "after full report <br>";
+          $servername = $_ENV['MYSQL_SERVER'];
+          $username = $_ENV["MYSQL_USERNAME"];
+          $password = $_ENV["MYSQL_PASSWORD"];
+          $dbname = $_ENV["MYSQL_DATABASE"];
+
+          $conn = new mysqli($servername, $username, $password, $dbname);
+          echo "Before db <br>";
+          if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }
+          $sql = "INSERT INTO `blocklist`(`ip`, `reason`) VALUES ('${ip}', 'Hacking attempt (HTTP)')";
+          $result = $conn->query($sql);
+          echo "After DB <br>";
+          
+        }
+
 $servername = $_ENV['MYSQL_SERVER'];
 $username = $_ENV["MYSQL_USERNAME"];
 $password = $_ENV["MYSQL_PASSWORD"];
@@ -997,55 +1047,7 @@ $router->set404(function() {
     echo "Under header <br>";
     
     
-    $url = $_SERVER["REQUEST_URI"];
-
     
-    $ua = $_SERVER['HTTP_USER_AGENT'];
-    echo "Before issets <br>";
-    $hacks = Secure::returnExploits();
-    if(isset($hacks[$url])){
-        $mes = "AUTOMATED REPORT: " . $hacks[$url];
-    }
-        echo "Before report <br>";
-    if(isset($mes)){
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $to_discord = "${ip} - ${mes}";
-        Rocks::newDiscord($to_discord, "Hacker Feed");
-        $client = new GuzzleHttp\Client([
-            'base_uri' => 'https://api.abuseipdb.com/api/v2/'
-          ]);
-          echo "Before POST <br>";
-          $response = $client->request('POST', 'report', [
-              'query' => [
-                  'ip' => "${ip}",
-                  'categories' => '15',
-                  'comment' => "${mes}"
-              ],
-              'headers' => [
-                  'Accept' => 'application/json',
-                  'Key' => $_ENV["ABUSE_IP_DB"]
-            ],
-          ]);
-          echo "After send <br>";
-          $output = $response->getBody();
-          // Store response as a PHP object.
-          $ipDetails = json_decode($output, true);
-          echo "after full report <br>";
-          $servername = $_ENV['MYSQL_SERVER'];
-          $username = $_ENV["MYSQL_USERNAME"];
-          $password = $_ENV["MYSQL_PASSWORD"];
-          $dbname = $_ENV["MYSQL_DATABASE"];
-
-          $conn = new mysqli($servername, $username, $password, $dbname);
-          echo "Before db <br>";
-          if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-          }
-          $sql = "INSERT INTO `blocklist`(`ip`, `reason`) VALUES ('${ip}', 'Hacking attempt (HTTP)')";
-          $result = $conn->query($sql);
-          echo "After DB <br>";
-          
-        }
 });
 
 
