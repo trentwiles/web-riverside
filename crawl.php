660680be4761f67e5501ef1a137e4e35395e4078
Riverside Rocks <trent@riverside.rocks>
2020-11-17 14:22:37 -0500

    Update crawl.php

--- crawl.php
+++ crawl.php
@@ -9,6 +9,7 @@ $crawl = $_GET["url"];
 $cURLConnection = curl_init();
 curl_setopt($cURLConnection, CURLOPT_URL, $crawl);
 curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+curl_setopt( $cURLConnection, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; Riverside Rocks / Wall Engine; +https://riverside.rocks)" );
 $content = curl_exec($cURLConnection);
 curl_close($cURLConnection);
 
