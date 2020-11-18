2ac81ddcfe7695d1d12e76d3ccf5300287fd5f87
Riverside Rocks <trent@riverside.rocks>
2020-11-17 19:49:28 -0500

    Revert a good deal of changes - wow

--- index.php
+++ index.php
@@ -1001,54 +1001,100 @@ $router->get('/account/dashboard', function() {
 
 $router->set404(function() {
     header('HTTP/1.1 404 Not Found');
-    echo "404: Not Found";
-    $ip = $_SERVER['REMOTE_ADDR'];
-$url = $_SERVER["REQUEST_URI"];
-        
-$ua = $_SERVER['HTTP_USER_AGENT'];
-$hacks = $exploits;
-
-echo $hacks[$url];
-
-if(isset($hacks[$url])){
-    $mes = "AUTOMATED REPORT: " . $hacks[$url];
-}
-if(isset($mes)){
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $to_discord = "${ip} - ${mes}";
-    $client = new GuzzleHttp\Client([
-        'base_uri' => 'https://api.abuseipdb.com/api/v2/'
-        ]);
-        $response = $client->request('POST', 'report', [
-            'query' => [
-                'ip' => "${ip}",
-                'categories' => '15',
-                'comment' => "${mes}"
-            ],
-            'headers' => [
-                'Accept' => 'application/json',
-                'Key' => $_ENV["ABUSE_IP_DB"]
-        ],
-        ]);
-        $output = $response->getBody();
-        // Store response as a PHP object.
-        $ipDetails = json_decode($output, true);
-        $servername = $_ENV['MYSQL_SERVER'];
-        $username = $_ENV["MYSQL_USERNAME"];
-        $password = $_ENV["MYSQL_PASSWORD"];
-        $dbname = $_ENV["MYSQL_DATABASE"];
-
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        }
-        $sql = "INSERT INTO `blocklist`(`ip`, `reason`) VALUES ('${ip}', 'Hacking attempt (HTTP)')";
-        $result = $conn->query($sql);
-        
-    }
-
-    // Where is this comming from??????????????????????????????????????????????????????
     
+    $hacks = Secure::returnExploits();
+    
+    $url = $_SERVER["REQUEST_URI"];
+    
+    $user_agent_blacklist = array(
+        "Go-http-client/1.1",
+        "Mozilla/5.0 zgrab/0.x",
+        "python-requests/2.24.0"
+    );
+    
+    $ua = $_SERVER['HTTP_USER_AGENT'];
+    
+    if(isset($hacks[$url]) || isset($baduseragent[$ua])){
+        $mes = "AUTOMATED REPORT: " . $hacks[$url];
+    }
+    
+    if(isset($baduseragent[$ua])){
+        $mes = "AUTOMATED REPORT: Port Scanning: " . $url;
+    }
+        
+    if(isset($mes)){
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $to_discord = "${ip} - ${mes}";
+        Rocks::newDiscord($to_discord, "Hacker Feed");
+        $client = new GuzzleHttp\Client([
+            'base_uri' => 'https://api.abuseipdb.com/api/v2/'
+          ]);
+          
+          $response = $client->request('POST', 'report', [
+              'query' => [
+                  'ip' => "${ip}",
+                  'categories' => '15',
+                  'comment' => "${mes}"
+              ],
+              'headers' => [
+                  'Accept' => 'application/json',
+                  'Key' => $_ENV["ABUSE_IP_DB"]
+            ],
+          ]);
+          
+          $output = $response->getBody();
+          // Store response as a PHP object.
+          $ipDetails = json_decode($output, true);
+          $servername = $_ENV['MYSQL_SERVER'];
+          $username = $_ENV["MYSQL_USERNAME"];
+          $password = $_ENV["MYSQL_PASSWORD"];
+          $dbname = $_ENV["MYSQL_DATABASE"];
+
+          $conn = new mysqli($servername, $username, $password, $dbname);
+          if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }
+          $sql = "INSERT INTO `blocklist` (`ip`, `reason`) VALUES ('${ip}','Hacking attempt (HTTP)')";
+          $result = $conn->query($sql);
+        }
+    // If a hacking attempt is detected, we show the 403 page
+    if(in_array($_SERVER["REQUEST_URI"], $hacks))
+    {
+        Phug::displayFile('views/403.pug');
+    }
+    else
+    {
+        Phug::displayFile('views/404.pug');
+    }
+    $list = Secure::userAgents();
+    if(in_array($ua, $list))
+    {
+        $mes = $list[$ua];
+        if(isset($mes)){
+            $ip = $_SERVER['REMOTE_ADDR'];
+            $to_discord = "${ip} - ${mes}";
+            Rocks::newDiscord($to_discord, "Hacker Feed");
+            $client = new GuzzleHttp\Client([
+                'base_uri' => 'https://api.abuseipdb.com/api/v2/'
+              ]);
+              
+              $response = $client->request('POST', 'report', [
+                  'query' => [
+                      'ip' => "${ip}",
+                      'categories' => '15',
+                      'comment' => "${mes}"
+                  ],
+                  'headers' => [
+                      'Accept' => 'application/json',
+                      'Key' => $_ENV["ABUSE_IP_DB"]
+                ],
+              ]);
+              
+              $output = $response->getBody();
+              // Store response as a PHP object.
+              $ipDetails = json_decode($output, true);
+            }
+    }
 });
 
 
