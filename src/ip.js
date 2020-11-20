34b070a60cd37bbb0cc9aeabcf40de8431fed718
Riverside Rocks <trent@riverside.rocks>
2020-11-19 20:45:47 -0500

    Update ip.js

--- src/ip.js
+++ src/ip.js
@@ -8,15 +8,15 @@ var channel = pusher.subscribe('abuseipdb');
 channel.bind('message', function(data) {
     var node = document.createElement("p");
     
-    var api_url = "https://ip-api.com/json/"+data.message;
+    var api_url = "https://ipapi.co/"+data.message+"/json/ "+;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var json = this.responseText
         var ip = JSON.parse(json)
         
-        var country = ip.country
-        var code = ip.countryCode
+        var country = ip.country_name
+        var code = ip.country_code
     }
     };
     xhttp.open("GET", api_url, true);
