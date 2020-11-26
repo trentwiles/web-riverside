03ddfb431b953ec2b21aea399d3f94ccd5bb6a30
Riverside Rocks <trent@riverside.rocks>
2020-11-25 20:31:34 -0500

    a wow

--- index.php
+++ index.php
@@ -176,6 +176,7 @@ $router->get('/api/bycountry', function() {
 });
 
 $router->get('/api/image', function() {
+    $this->random = new PragmaRX\Random\Random();
     $servername = $_ENV['MYSQL_SERVER'];
     $username = $_ENV["MYSQL_USERNAME"];
     $password = $_ENV["MYSQL_PASSWORD"];
@@ -197,10 +198,11 @@ $router->get('/api/image', function() {
 
     $sql = "SELECT * FROM uploads";
     $result = $conn->query($sql);
+    $wow = $this->random->numeric()->start(0)->end($ammount)->get();
     $ammount = 0;
     if (!empty($result) && $result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            if($ammount = $upload)
+            if($ammount = $wow)
             {
                 $image = $row["url"];
                 break;
