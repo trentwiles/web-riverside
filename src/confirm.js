8ff9cde92a4cc83e2b143eedeaf9346e30095f19
Riverside Rocks <trent@riverside.rocks>
2020-11-29 09:28:13 -0500

    Add confirm script

---
+++ src/confirm.js
@@ -0,0 +1,8 @@
+if(Cookies.get('seen') == ""){
+    var alertContent = "Thanks for stopping by. If you would like to opt out of our analytics program, please click <a onclick='Cookies.set('lytics', 'false')' class='alert-link'>here.</a>";
+    halfmoon.initStickyAlert({
+      content: alertContent,      
+      title: "Analytics"      
+    })
+    Cookies.set('seen', 'true')
+}
