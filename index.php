470d9613bd677ac12d1b80bf4c35b003905ba54c
Riverside Rocks <trent@riverside.rocks>
2020-12-03 19:05:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -177,8 +177,10 @@ $router->get('/api/bycountry', function() {
 });
 
 $router->get('/api/cidr', function() {
-    $ipi = new IP($_GET["ip"]);
-    print_r(IP('1.1.1.0/24')->hosts);
+    $hosts = Network::parse('192.168.1.0/24')->hosts; // Range(192.168.1.1, 192.168.1.254);
+    foreach($hosts as $ip) {
+        echo (string)$ip . '<br>';
+    }
 }
 
 
