a91a5726c55a347fbfdeb778933dd6a0a3084385
Riverside Rocks <trent@riverside.rocks>
2020-12-22 11:43:52 -0500

    File_get_contents instead of include

--- index.php
+++ index.php
@@ -546,7 +546,7 @@ $router->get('/admin/upload', function() {
 });
 
 $router->get('/v1/analytics', function() {
-    include "analyics.php";
+    file_get_contents("https://riverside.rocks/analyics.php");
 });
 
 
