2f68821799beca2274f9971237b96698348f84a0
Riverside Rocks <trent@riverside.rocks>
2020-12-28 16:39:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ if (!empty($result) && $result->num_rows > 0) {
 if($blocks !== 0)
 {
     header("HTTP/1.1 403 Forbidden");
-    echo "<center><br><br><br><img src='https://cdn.riverside.rocks/a/purchase-devourer-harrier.png' height='480px' width='1300px'/>";
+    echo "<center><br><br><br><img src='https://cdn.riverside.rocks/a/purchase-devourer-harrier.png' height='480px' width='1100px'/>";
     die();
 }
 
